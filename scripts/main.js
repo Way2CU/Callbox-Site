03e98f29ece11d08e0716f03a8d0f5b7ad418d74
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-08-21 20:56:37 +0300

    Initial import.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,28 @@
 /**
  * Main JavaScript
- * Site Name
+ * Callbox
  *
  * Copyright (c) 2014. by Way2CU, http://way2cu.com
- * Authors:
+ * Authors: Mladen Mijatov
  */
 
+var Caracal = Caracal || {};
+
+
+function handle_window_scroll(event) {
+	var scroll = $(this).scrollTop();
+
+	if (scroll > 0 && !Caracal.header.hasClass('detached')) {
+		Caracal.header.addClass('detached');
+
+	} else if (scroll == 0 && Caracal.header.hasClass('detached')) {
+		Caracal.header.removeClass('detached');
+	}
+}
+
 function on_site_load() {
+	Caracal.header = $('header');
+	$(window).scroll(handle_window_scroll);
 }
 
 $(on_site_load);
