261f6df2a3e78f59bc094b542b76983cbc8563aa
Khalid Sawalhi <khalid@way2cu.com>
2015-01-28 12:25:11 +0200

    Added testimonials script.

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,18 @@ function handle_window_scroll(event) {
 function on_site_load() {
 	Caracal.header = $('header');
 	$(window).scroll(handle_window_scroll);
+
+	// Testimonials Home Page Clients Said
+
+		if ($('div.testimonial').length > 0) {
+			Caracal.testimonial_pages = new PageControl('div.testimonials_container', 'div.testimonial')
+				.setInterval(10000)
+				.setWrapAround(true)
+				.setPauseOnHover(true)
+				.attachNextControl($('div.testimonials_container a.arrow.next'))
+				.attachPreviousControl($('div.testimonials_container a.arrow.previous'));
+		}
+
 }
 
 $(on_site_load);
